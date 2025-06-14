ac6c1fda1fc67a2cff11b91c4bbcdc64cba07b49
Vinh Nguyen <ngtv0404@gmail.com>
2025-06-14 11:18:53 +0700

    rename lidar_link to base_scan

--- launch/bringup.launch.py
+++ launch/bringup.launch.py
@@ -112,6 +112,12 @@ def generate_launch_description():
         output="both",
         arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'base_footprint']
     )
+    base_scan_pub_node = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        output="both",
+        arguments=['0', '0', '0.255', '0', '0', '0', 'base_link', 'base_scan']
+    )
 
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
@@ -148,7 +154,8 @@ def generate_launch_description():
         robot_controller_spawner,
         delay_rviz_after_joint_state_broadcaster_spawner,
         delay_joint_state_broadcaster_after_robot_controller_spawner,
-        base_footprint_pub_node
+        base_footprint_pub_node,
+        base_scan_pub_node
     ]
 
     return LaunchDescription(declared_arguments + nodes)
